ed4c1922681554731f3c647c77a0e2abb8bc644f
digaridivyansh-create <digaridivyansh@gmail.com>
2026-02-25 10:15:16 +0530

    Add

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //console.log("Hello World");
-var num=10;
+/*var num=10;
 console.log(num);
 num =false;
 console.log(typeof(num));
@@ -10,4 +10,30 @@ console.log(typeof(de));
 res= null;
 console.log(typeof(res));
 des=undefined;
-console.log(typeof(des));
+console.log(typeof(des));*/
+/*var person={
+    name:"barack obama",
+    age:51,
+    title:'44th president of states'
+}*/
+/*const sym1=Symbol(4);
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("false");
+}*/
+let a=10;
+++a;
+let b=25;
+++b;
+console.log(a);
+console.log(b);
+
+
+
+
+
