bd321e55fa81e5355921239153f8e687428fb260
digaridivyansh-create <digaridivyansh@gmail.com>
2026-03-11 09:41:28 +0530

    add

---
+++ lec2.js
@@ -0,0 +1,35 @@
+/*let count=0;
+console.log("starting loop"+"<br/>");
+for(count=0;count<10;count++){
+    console.log("current count;"+count);
+    console.log("</br>");
+}
+console.log("loop started");*/
+
+/*const person={
+    fnae:"ajay",
+    lname:"kumar",
+    age:25,
+};
+for(x in person){
+    console.log("person details:"+x+":"+person[x]);
+}*/
+
+/*var age = 20;
+if (age > 18) {
+  console.log("<b>you are eligible to vote</b>");
+} else {
+  console.log("you are a minor");
+}*/
+
+var grade='A';
+document.write("entering switch block"+"<br/>");
+switch(grade){
+    case 'A':document.write("good job</br>");
+    break;
+    case 'B':document.write("prettier good</br>");
+    break;
+    case 'C':document.write("passed</br>");
+    break;
+    default:document.write("invalid grade</br>");
+}
